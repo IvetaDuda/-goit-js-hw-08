327f0bf1e48feac76d1fc22f9b15ef7033dfccfb
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-05-05 07:24:58 +0000

    Deploying to gh-pages from @ IvetaDuda/-goit-js-hw-08@5a72edf17f9207f41dfcdfa00dcd7e6e2087c3cd 🚀

--- 01-gallery.188075f0.js
+++ 01-gallery.188075f0.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const t=document.querySelector(".gallery");function i(l){return e.galleryItems.map(({preview:e,original:l,description:r})=>`<a class="gallery__item" href="${l}">\n        <img\n          class="gallery__image"\n          src="${e}"\n          alt="${r}"\n        />\n      </a>`).join("")}t.insertAdjacentHTML("beforeend",i(e.galleryItems));const a=new l.default(".gallery a",{captionsData:"alt",captionDelay:250});
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.188075f0.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.188075f0.js.map
